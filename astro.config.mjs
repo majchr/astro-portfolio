738e0ce85f97fd47041c54cfc0919e33da0e7458
Abdelmajid Chroud <mchroud@gmail.com>
2026-07-01 12:40:23 +0200

    Ajout sitemap SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from "@astrojs/sitemap";
+
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-portfolio.mchroud.workers.dev",
+  integrations: [sitemap()],
   adapter: cloudflare()
 });
